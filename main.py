7cafec249ef059db941bf5f1188436d54ae8f3af
nishukothari <nishuk@umich.edu>
2021-02-04 20:02:36 -0500

    restructured after brainstorm

---
+++ main.py
@@ -0,0 +1,9 @@
+def train_test():
+    NotImplemented
+    
+def main():
+    # Placeholder - Add I/O handling to customize input to the training and testing function
+    NotImplemented
+
+if __name__ == "__main__":
+    main()
